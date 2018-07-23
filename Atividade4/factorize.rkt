025a156cc404e7b438dc10e4287b2d17d35e7431
Andre <Andre>
2018-07-22 22:06:59 -0300

    esqueleto da 1b a fazer

--- Atividade4/factorize.rkt
+++ Atividade4/factorize.rkt
@@ -7,9 +7,16 @@
 
 (define arg0 (string->number (vector-ref args 0)))
 (define arg1 (vector-ref args 1))
+(define arg2 (vector-ref args 2))
 
 (define (abre-arquivo-in arq) (open-input-file arq))
 (define (abre-arquivo-out arq) (open-output-file arq #:exists 'append))
 
+
 (define numero-alvo arg0)
-(define arquivo-alvo arg1)
+(define arquivo-entrada arg1)
+(define arquivo-saida arg2)
+
+(define (escreve t) (write t (abre-arquivo-out arquivo-saida)))
+(define (leia) (read (abre-arquivo-in arquivo-entrada)))
+
